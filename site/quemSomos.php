d84fdf1fff7f148d03d68de331c8ed950b258378
Cleissonp <cleissonp@gmail.com>
2018-04-29 19:35:30 -0300

    Quem Somos
    Pagina "Quem Somos" desenvolvida, de maneira estática sem Fuctions do PHP,

---
+++ site/quemSomos.php
@@ -0,0 +1,163 @@
+<?php include 'includes/estrutura/assinatura.php'; ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <!-- Informações básicas
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>QUEM SOMOS</title>
+    <?php include 'includes/estrutura/head.php'; ?>
+
+</head>
+
+<body>
+
+    <!-- Menu de Navegação
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/estrutura/navbar.php'; ?>
+
+    <!-- Header
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/home-header.php'; ?>
+
+    <!-- Conteúdo
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <main>
+        <article class="escolas">
+            <h1 style="display: none">Conteúdo</h1>
+            <!-- Alunos -->
+            <div class="container">
+                <div class="section">
+                    <!-- Coluna 1
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+                    <div class="col-xs-12 col-md-12">
+                        <!-- Primeira linha
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+                        <div class="row SobrePortal ">
+                            <h1>Sobre o Portal</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum repellat repudiandae voluptates adipisci ad fugiat perspiciatis impedit consequatur nisi reiciendis eum sunt eligendi et commodi expedita, explicabo soluta, corporis quis ipsa, illo, incidunt vero! </p>
+                        </div>
+                        <div class="container divisor escola2  extra-space"></div>
+                        <!-- Segunda  linha
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+                        <div class="row equipe">
+                            <h1 class="extra-space">A Equipe</h1>
+                            <!--Integrantes-->
+                            <div class="col-xs-12 col-md-4 extra-space">
+
+
+                                <div class="thumbnail materia escola1">
+                                    <img src="http://via.placeholder.com/150x150" alt="..." class="img-circle">
+                                    <div class="caption">
+                                        <h1>Nome dos Integrantes
+                                            <small>RA:000.000.0000.0000</small>
+                                        </h1>
+                                        <h3>Cargo</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, fuga cum dolores architecto quo voluptatum saepe id perferendis! Quae, minus, culpa.</p>
+                                        <p>
+                                            <strong>Area Responsalvel</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Integrantes-->
+                            <div class="col-xs-12 col-md-4 extra-space">
+
+                                <div class="thumbnail materia escola2">
+                                    <img src="http://via.placeholder.com/150x150" alt="..." class="img-circle">
+                                    <div class="caption">
+                                        <h1>Nome dos Integrantes
+                                            <small>RA:000.000.0000.0000</small>
+                                        </h1>
+                                        <h3>Cargo</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, fuga cum dolores architecto quo voluptatum saepe id perferendis! Quae, minus, culpa.</p>
+                                        <p>
+                                            <strong>Area Responsalvel</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Integrantes-->
+                            <div class="col-xs-12 col-md-4 extra-space">
+
+                                <div class="thumbnail materia escola3">
+                                    <img src="http://via.placeholder.com/150x150" alt="..." class="img-circle">
+                                    <div class="caption">
+                                        <h1>Nome dos Integrantes
+                                            <small>RA:000.000.0000.0000</small>
+                                        </h1>
+                                        <h3>Cargo</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, fuga cum dolores architecto quo voluptatum saepe id perferendis! Quae, minus, culpa.</p>
+                                        <p>
+                                            <strong>Area Responsalvel</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Integrantes-->
+                            <div class="col-xs-12 col-md-4 extra-space">
+
+                                <div class="thumbnail materia escola4 extra-space">
+                                    <img src="http://via.placeholder.com/150x150" alt="..." class="img-circle">
+                                    <div class="caption">
+                                        <h1>Nome dos Integrantes
+                                            <small>RA:000.000.0000.0000</small>
+                                        </h1>
+                                        <h3>Cargo</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, fuga cum dolores architecto quo voluptatum saepe id perferendis! Quae, minus, culpa.</p>
+                                        <p>
+                                            <strong>Area Responsalvel</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Integrantes-->
+                            <div class="col-xs-12 col-md-4 extra-space">
+
+                                <div class="thumbnail materia escola5">
+                                    <img src="http://via.placeholder.com/150x150" alt="..." class="img-circle">
+                                    <div class="caption">
+                                        <h1>Nome dos Integrantes
+                                            <small>RA:000.000.0000.0000</small>
+                                        </h1>
+                                        <h3>Cargo</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, fuga cum dolores architecto quo voluptatum saepe id perferendis! Quae, minus, culpa.</p>
+                                        <p>
+                                            <strong>Area Responsalvel</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Integrantes-->
+                            <div class="col-xs-12 col-md-4 extra-space">
+                                <div class="thumbnail materia escola6">
+                                    <img src="http://via.placeholder.com/150x150" alt="..." class="img-circle">
+                                    <div class="caption">
+                                        <h1>Nome dos Integrantes
+                                            <small>RA:000.000.0000.0000</small>
+                                        </h1>
+                                        <h3>Cargo</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, fuga cum dolores architecto quo voluptatum saepe id perferendis! Quae, minus, culpa.</p>
+                                        <p>
+                                            <strong>Area Responsalvel</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </main>
+
+    <!-- Rodapé
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/estrutura/footer.php'; ?>
+
+</body>
+
+</html>
