f4a455b91b99d7e98d8ea395e4e85324acab7acb
Cleissonp <cleissonp@gmail.com>
2018-05-01 22:42:24 -0300

    Quem Somos
    Adicionado Function PHP, e alteraçoes visuais

---
+++ site/includes/quemSomos-content.php
@@ -0,0 +1,127 @@
+<?php 
+
+include 'includes/functions/quemSomos-sections.php'; 
+
+// sequencia de declaração:
+// escola (escola1, escola2, escola3, escola4, escola5, escola6)
+// Imagem de Perfil (localização da Imagem de Perfil)
+// Nome (Nome do Integrante)
+// RA (Ra do Integrante)
+// Cargo (Qual o Cargo do Integrante)
+// Descrição Pessoal (Descrição Pessoal)
+// Responsabilidades (Qual parte voce é Responsalvel)
+
+// Integrante 1
+//----------------------------------------------------//
+quemSomos1(
+    // escola
+    'escola1',
+    // Imagem Perfil
+    'http://via.placeholder.com/125x125',
+    // Nome
+    'Nome Aqui.',
+    // RA
+    '000.0000.000.000.000',
+    // Cargo
+    'Cargo teste',
+    // Descriçao pessoal
+    'Evidentemente, o início da atividade geral de formação de atitudes apresenta tendências no sentido de aprovar a manutenção das condições financeiras e administrativas exigidas.',
+    // Responsabilidades
+    'Foi feitas as paginas x e y.'
+);
+
+// Integrante 2
+//----------------------------------------------------//
+quemSomos2(
+    // escola
+    'escola2',
+    // Imagem Perfil
+    'http://via.placeholder.com/125x125',
+    // Nome
+    'Nome Aqui.',
+    // RA
+    '000.0000.000.000.000',
+    // Cargo
+    'Cargo teste',
+    // Descriçao pessoal
+    'Evidentemente, o início da atividade geral de formação de atitudes apresenta tendências no sentido de aprovar a manutenção das condições financeiras e administrativas exigidas.',
+    // Responsabilidades
+    'Foi feitas as paginas x e y.'
+);
+
+// Integrante 3
+//----------------------------------------------------//
+quemSomos3(
+    // escola
+    'escola3',
+    // Imagem Perfil
+    'http://via.placeholder.com/125x125',
+    // Nome
+    'Nome Aqui.',
+    // RA
+    '000.0000.000.000.000',
+    // Cargo
+    'Cargo teste',
+    // Descriçao pessoal
+    'Evidentemente, o início da atividade geral de formação de atitudes apresenta tendências no sentido de aprovar a manutenção das condições financeiras e administrativas exigidas.',
+    // Responsabilidades
+    'Foi feitas as paginas x e y.'
+);
+
+// Integrante 4
+//----------------------------------------------------//
+quemSomos4(
+    // escola
+    'escola4',
+    // Imagem Perfil
+    'http://via.placeholder.com/125x125',
+    // Nome
+    'Nome Aqui.',
+    // RA
+    '000.0000.000.000.000',
+    // Cargo
+    'Cargo teste',
+    // Descriçao pessoal
+    'Evidentemente, o início da atividade geral de formação de atitudes apresenta tendências no sentido de aprovar a manutenção das condições financeiras e administrativas exigidas.',
+    // Responsabilidades
+    'Foi feitas as paginas x e y.'
+);
+
+// Integrante 5
+//----------------------------------------------------//
+quemSomos5(
+    // escola
+    'escola5',
+    // Imagem Perfil
+    'http://via.placeholder.com/125x125',
+    // Nome
+    'Nome Aqui.',
+    // RA
+    '000.0000.000.000.000',
+    // Cargo
+    'Cargo teste',
+    // Descriçao pessoal
+    'Evidentemente, o início da atividade geral de formação de atitudes apresenta tendências no sentido de aprovar a manutenção das condições financeiras e administrativas exigidas.',
+    // Responsabilidades
+    'Foi feitas as paginas x e y.'
+);
+// Integrante 6
+//----------------------------------------------------//
+quemSomos6(
+    // escola
+    'escola6',
+    // Imagem Perfil
+    'http://via.placeholder.com/125x125',
+    // Nome
+    'Nome Aqui.',
+    // RA
+    '000.0000.000.000.000',
+    // Cargo
+    'Cargo teste',
+    // Descriçao pessoal
+    'Evidentemente, o início da atividade geral de formação de atitudes apresenta tendências no sentido de aprovar a manutenção das condições financeiras e administrativas exigidas.',
+    // Responsabilidades
+    'Foi feitas as paginas x e y.'
+);
+    
+?>
