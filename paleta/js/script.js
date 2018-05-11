37e1e684b0f2a2f946aaeb6dfbd02de8bd756ca0
Filipe Daineze <jimmyfilips@gmail.com>
2018-05-11 00:27:13 -0300

    paleta

---
+++ paleta/js/script.js
@@ -0,0 +1,42 @@
+function trocaCorNav(){
+   var cor = document.getElementById("cornav").value;
+   document.getElementById("navbar").style.backgroundColor = cor;
+}
+function trocaCorHeader(){
+   var cor = document.getElementById("corheader").value;
+   document.getElementById("header").style.backgroundColor = cor;
+}
+function trocaCorCorpo(){
+   var cor = document.getElementById("corcorpo").value;
+   document.getElementById("corpo").style.backgroundColor = cor;
+}
+function trocaCorFooter(){
+   var cor = document.getElementById("corfooter").value;
+   document.getElementById("footer").style.backgroundColor = cor;
+}
+
+
+function trocaCorEscola1(){
+   var cor = document.getElementById("corescola1").value;
+   document.getElementById("escola1").style.borderLeftColor = cor;
+}
+function trocaCorEscola2(){
+   var cor = document.getElementById("corescola2").value;
+   document.getElementById("escola2").style.borderLeftColor = cor;
+}
+function trocaCorEscola3(){
+   var cor = document.getElementById("corescola3").value;
+   document.getElementById("escola3").style.borderLeftColor = cor;
+}
+function trocaCorEscola4(){
+   var cor = document.getElementById("corescola4").value;
+   document.getElementById("escola4").style.borderLeftColor = cor;
+}
+function trocaCorEscola5(){
+   var cor = document.getElementById("corescola5").value;
+   document.getElementById("escola5").style.borderLeftColor = cor;
+}
+function trocaCorEscola6(){
+   var cor = document.getElementById("corescola6").value;
+   document.getElementById("escola6").style.borderLeftColor = cor;
+}
