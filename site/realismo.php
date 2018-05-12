62afca9b8eaba4034efe346f1af8d389a0d5e7c7
luizahelena <luizacollo@gmail.com>
2018-05-12 12:22:15 -0300

    Minhas páginas

---
+++ site/realismo.php
@@ -0,0 +1,62 @@
+<?php include 'includes/estrutura/assinatura.php'; ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head> 
+   
+    <!-- Informações básicas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>TITULO DO SITE</title>
+    <?php include 'includes/estrutura/head.php'; ?>
+    <link rel="stylesheet" href="css/realismo.css">
+    
+</head>
+
+<body>
+
+    <!-- Menu de Navegação
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/estrutura/navbar.php'; ?>
+
+    <!-- Header
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/home-header.php'; ?>    
+
+    <!-- Conteúdo
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <main>
+        <h1 style="display: none">Conteúdo</h1>
+        
+        <!-- Escolas Literárias
+        –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="container">
+<div class="row">
+<h1 class="paddingh1">Contexto Histórico do Realismo</h1>
+<article class="col-xs-12 col-md-6 ">
+  
+ <div class=" col-xs-12 col-md-12 paddingimg">  <img src="img/obrarealista.jpg" alt="Descrição" class="descricaoimg">
+   <p>"Pintura do Baile da Ilha Fiscal, o último da monarquia brasileira." - Livro 1889.</p>
+   </div> 
+    <p class="pararealismo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis.</p>
+    <p class="pararealismo">Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.</p>
+     <p class="pararealismo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat dignissimos voluptatem laudantium, quaerat possimus error odit distinctio omnis consectetur, animi quidem, id nihil ipsam repellat fugit quibusdam ex magnam asperiores.</p>
+</article>    
+    <article class="col-xs-12 col-md-6">
+         <p class="pararealismo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.</p>
+         <p class="pararealismo"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. Officia, non, voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis.  Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis.Officia, non, voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum est sunt eligendi molestiae nemo blanditiis, quisquam maiores aut quam ducimus nulla ullam magnam corporis sapiente in reiciendis. </p>
+    </article>
+</div>
+           
+</div>
+        
+    </main>
+
+    <!-- Rodapé
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/estrutura/footer.php'; ?>   
+
+</body>
+
+</html>
---
+++ site/realismo2.php
@@ -0,0 +1,74 @@
+<?php include 'includes/estrutura/assinatura.php'; ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <!-- Informações básicas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>TITULO DO SITE</title>
+    <?php include 'includes/estrutura/head.php'; ?>
+    <link rel="stylesheet" href="css/realismo.css">
+</head>
+
+<body>
+
+    <!-- Menu de Navegação
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/estrutura/navbar.php'; ?>
+
+    <!-- Header
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/home-header.php'; ?>
+
+    <!-- Conteúdo
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <main>
+        <h1 style="display: none">Conteúdo</h1>
+
+        <!-- Escolas Literárias
+        –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <div class="container">
+            <div class="row">
+                <h1>Realismo no Brasil </h1>
+                <article class="col-xs-12 col-md-6 col-lg-6">
+                    <p class="pararealismo">Aliquam erat volutpat. Phasellus semper cursus suscipit. Curabitur eget lacus finibus, porta metus id, tincidunt velit. Morbi ac augue ac libero rhoncus rhoncus. Nullam mattis semper magna, non vulputate lorem. Fusce quis tortor semper, congue est sit amet, ornare nibh. Fusce et quam quis urna aliquam finibus. Morbi nibh purus, mattis nec ligula eget, fermentum venenatis elit. Maecenas dictum massa ut aliquet sodales. Sed ut fermentum leo. Nulla lobortis, justo vestibulum gravida fringilla, ligula odio ullamcorper ligula, ut aliquam elit libero eget nulla. Quisque vitae velit accumsan, bibendum ante vel, tempor augue. Proin eget erat quis nibh malesuada suscipit quis ac est.</p>
+                    <p class="pararealismo">Nulla metus magna, suscipit sed scelerisque quis, pretium vitae turpis. Mauris consectetur consequat nisi vel pretium. Praesent tristique leo sed elit condimentum, id vulputate leo elementum. Aliquam consectetur sapien posuere ex finibus fringilla. Quisque faucibus, diam sit amet lacinia pharetra, nibh lacus blandit lacus, sagittis porta magna diam vel ligula. Suspendisse semper suscipit finibus. Maecenas hendrerit est tortor, a volutpat odio ornare id. Duis sit amet quam volutpat, consequat ante non, dapibus erat. Integer aliquet, eros id facilisis lobortis, orci tortor dictum ligula, in finibus felis leo in neque. Suspendisse non sem sapien. Sed non lectus at ipsum ornare convallis at vitae mauris. Donec gravida neque vitae mollis ultrices. Proin a velit non est sollicitudin faucibus.</p>
+                    <p class="pararealismo">Nulla efficitur imperdiet tortor, ac dapibus libero dictum non. Donec id tortor et mauris condimentum sodales. In varius posuere nunc, eget tincidunt mauris blandit a. Nullam ut nunc nulla. Morbi convallis turpis porttitor, gravida libero ac.</p>
+                </article>
+                <aside class="col-xs-12 col-md-6 col-lg-6">
+                        <p class="pararealismo">Nunc sodales ornare eros. Praesent feugiat semper sollicitudin. Morbi egestas lorem leo, vitae feugiat nisl aliquam nec. Morbi dictum nisl neque. Donec pulvinar ullamcorper bibendum. Cras consequat lorem urna, id placerat risus feugiat at. Fusce vel libero non eros iaculis ultrices nec vitae nulla. Aliquam sollicitudin sem ac mi molestie, id pulvinar dui facilisis.</p>
+                    <p class="pararealismo">Duis vel vehicula felis. Sed mattis non massa non ornare. Curabitur sed accumsan dui. Nam a magna finibus, gravida diam vel, aliquam felis. Nulla facilisi. Nulla finibus sed tortor non imperdiet. Mauris commodo tortor id tellus vehicula tempus. Nunc ornare volutpat dui at volutpat. Aliquam a risus malesuada, sodales justo sit amet, posuere mi. Suspendisse euismod faucibus nibh, in aliquam libero consequat et. Maecenas placerat malesuada metus sed blandit. Fusce iaculis dictum justo et imperdiet. Maecenas in tellus pharetra, ultrices enim non, ornare leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec at augue sapien. Quisque varius porta ex.</p>
+                    <p class="pararealismo">Maecenas sem lorem, luctus sit amet finibus ut, lobortis eu orci. Mauris lobortis at ipsum quis sodales. Nullam porta auctor sem, at pharetra augue fermentum id. Fusce nec orci eget nisl laoreet elementum et eu massa. Sed porttitor libero vitae ex feugiat, at imperdiet lacus convallis. Ut sapien eros, tincidunt et porttitor vitae, fringilla vulputate justo. Aenean vel commodo neque, eget mollis eros. Mauris posuere tincidunt sapien at suscipit. Nam tempus ornare arcu in accumsan. Sed vitae fringilla orci. Aliquam neque erat, lacinia eu dictum et, molestie in tortor. Suspendisse posuere at dolor at mattis. Quisque nec ornare metus.</p>
+                </aside>
+
+                   <h1>Principais obras realistas</h1>
+                       <section class="col-xs-12 col-md-4 col-lg-4">
+                   <div class="col-xs-12 col-md-12 col-lg-12"><img src="http://placehold.it/70x100" class="col-xs-12 col-md-12 text-center" ></div>
+                <p class="pararealismo">Nunc sodales ornare eros. Praesent feugiat semper sollicitudin. Morbi egestas lorem leo, vitae feugiat nisl aliquam nec. Morbi dictum nisl neque. Donec pulvinar ullamcorper bibendum. Cras consequat lorem urna, id placerat risus feugiat at. Fusce vel libero non eros iaculis ultrices nec vitae nulla. Aliquam sollicitudin sem ac mi molestie, id pulvinar dui facilisis.</p>
+                   </section>
+                       <section class="col-xs-12 col-md-4 col-lg-4">
+                   <div class="col-xs-12 col-md-12 col-lg-12"><img src="http://placehold.it/70x100" class="col-xs-12 col-md-12" ></div>
+                <p class="pararealismo">Nunc sodales ornare eros. Praesent feugiat semper sollicitudin. Morbi egestas lorem leo, vitae feugiat nisl aliquam nec. Morbi dictum nisl neque. Donec pulvinar ullamcorper bibendum. Cras consequat lorem urna, id placerat risus feugiat at. Fusce vel libero non eros iaculis ultrices nec vitae nulla. Aliquam sollicitudin sem ac mi molestie, id pulvinar dui facilisis.</p>
+                   </section>
+                       <section class="col-xs-12 col-md-4 col-lg-4">
+                   <div class="col-xs-12 col-md-12 col-lg-12"><img src="http://placehold.it/70x100" class="col-xs-12 col-md-12 text-center"></div>
+                <p class="pararealismo">Nunc sodales ornare eros. Praesent feugiat semper sollicitudin. Morbi egestas lorem leo, vitae feugiat nisl aliquam nec. Morbi dictum nisl neque. Donec pulvinar ullamcorper bibendum. Cras consequat lorem urna, id placerat risus feugiat at. Fusce vel libero non eros iaculis ultrices nec vitae nulla. Aliquam sollicitudin sem ac mi molestie, id pulvinar dui facilisis.</p>
+                   </section>
+
+            </div>
+        </div>
+
+
+
+    </main>
+
+    <!-- Rodapé
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include 'includes/estrutura/footer.php'; ?>
+
+</body>
+
+</html>
