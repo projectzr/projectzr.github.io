0a8637ceaf60c44e484007ad4d7f9b3ef3e7fd29
Cleissonp <cleissonp@gmail.com>
2018-05-15 16:18:56 -0300

    Imagem
    @luizahelena  redeifnido resolução de imagem

--- site/contato.php
+++ site/contato.php
@@ -3,14 +3,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head> 
-   
+<head>
+
     <!-- Informações básicas
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
     <title>Brasil Literário - Contato</title>
     <?php include 'includes/estrutura/head.php'; ?>
-    
+
 </head>
 
 <body>
@@ -27,51 +27,60 @@
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <main>
         <h1 style="display: none">Conteúdo</h1>
-        
+
         <!-- Escolas Literárias
         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<div class="container section text-left">
-<div class="row text-left">
-<aside class="col-xs-12 col-md-6 col-lg-6 ">
-<h1>Informações</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet suscipit ante ac pulvinar. Cras vel mi ullamcorper, bibendum mi nec, posuere neque. Integer tristique neque metus, ut dapibus est tristique non. Sed eget magna tempor eros bibendum mollis ac nec ligula.</p>
-     <p>Curabitur molestie ipsum et mi rutrum pellentesque. Donec vestibulum diam vitae convallis rhoncus. Curabitur vel porta elit. Nulla vel libero mattis, maximus dolor vel, eleifend tortor. Maecenas fringilla nisi in vulputate hendrerit.</p>
-    <p>Praesent gravida posuere ante, id molestie tellus sollicitudin in. Sed sit amet lacus porttitor, maximus lacus nec, consectetur orci. Nam facilisis vulputate rutrum. Pellentesque non dui vitae lectus finibus porttitor. Donec id elit congue, euismod dolor a, auctor libero. Vestibulum tincidunt ligula et massa sollicitudin, id maximus turpis faucibus.Sed sit amet lacus porttitor, maximus lacus nec, consectetur orci.  </p>
-</aside>    
-<article class="col-xs-12 col-md-6 col-lg-6 text-left">
-<h2 style="display: none"></h2>
-<form>
- 
-			<div class="form-group">
-				<label class="classecontato" for="Nome">Nome:</label>
-				<input type="text" class="form-control" name="Nome" required>
-			</div>
- 
-			<div class="form-group">
-				<label for="Email">Email:</label>
-				<input type="email" class="form-control" name="Email" required>
-			</div>
+        <div class="container section text-left">
+            <div class="row text-left">
+                <article class="col-xs-12 col-md-6 col-lg-6 ">
+                    <h2 class="h1">Informações</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet suscipit ante ac pulvinar. Cras
+                        vel mi ullamcorper, bibendum mi nec, posuere neque. Integer tristique neque metus, ut dapibus est
+                        tristique non. Sed eget magna tempor eros bibendum mollis ac nec ligula.</p>
+                    <p>Curabitur molestie ipsum et mi rutrum pellentesque. Donec vestibulum diam vitae convallis rhoncus. Curabitur
+                        vel porta elit. Nulla vel libero mattis, maximus dolor vel, eleifend tortor. Maecenas fringilla nisi
+                        in vulputate hendrerit.</p>
+                    <p>Praesent gravida posuere ante, id molestie tellus sollicitudin in. Sed sit amet lacus porttitor, maximus
+                        lacus nec, consectetur orci. Nam facilisis vulputate rutrum. Pellentesque non dui vitae lectus finibus
+                        porttitor. Donec id elit congue, euismod dolor a, auctor libero. Vestibulum tincidunt ligula et massa
+                        sollicitudin, id maximus turpis faucibus.Sed sit amet lacus porttitor, maximus lacus nec, consectetur
+                        orci. </p>
+                </article>
+                <article class="col-xs-12 col-md-6 col-lg-6 text-left">
+                    <h2 style="display: none">OK</h2>
+                    <form>
 
-			<div class="form-group">
-				<label for="Telefone">Telefone:</label>
-				<input type="number" class="form-control" name="Telefone" min="0" max="11" required>
-			</div>
-<div class="form-group">
-    <label for="exampleFormControlTextarea2">Mensagem:</label>
-    <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="5"></textarea>
-</div>
-		<div class="form-group"	> <button type="submit" class="btn btn-primary">Enviar</button></div>
-    </form>
-</article>
-</div>
-           
-</div>
-        
+                        <div class="form-group">
+                            <label class="classecontato" for="nome">Nome:</label>
+                            <input type="text" class="form-control" name="nome" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" class="form-control" name="email" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="tel">Telefone:</label>
+                            <input type="number" class="form-control" name="tel" min="0" max="11" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea2">Mensagem:</label>
+                            <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="5"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </form>
+                </article>
+            </div>
+        </div>
+
     </main>
 
     <!-- Rodapé
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <?php include 'includes/estrutura/footer.php'; ?>   
+    <?php include 'includes/estrutura/footer.php'; ?>
 
 </body>
 
--- site/simbolismo-autor-destaque.php
+++ site/simbolismo-autor-destaque.php
@@ -22,7 +22,9 @@
 
   <!-- Header
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <?php $titulo='Simbolismo'; $subtitulo='Cruz e Souza'; include 'includes/section-header.php'; ?>
+  <?php $titulo='Simbolismo'; 
+  // $subtitulo='Cruz e Souza';
+   include 'includes/section-header.php'; ?>
 
   <!-- Conteúdo
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -32,16 +34,18 @@
     <!-- Escolas Literárias
         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <div class="container-fluid">
-
       <div class="row">
+        <h2 class="titulo text-center">Cruz e Souza</h2>
         <hr class="hidden-xs">
         <div class="media">
           <div class="media-left media-middle">
             <img class="media-object  center-block" src="https://placehold.it/544x600?text=Autor-Destaque" alt="Autor de Destaque ">
           </div>
           <div class="media-body media-middle hidden-xs">
-            <h2>Simbolismo</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem et necessitatibus, ea saepe quas optio perspiciatis
+              voluptas pariatur cupiditate, odio sapiente, aspernatur hic. Nulla aut vel sunt veniam autem quis nam ipsum
+              quasi molestias velit! ea saepe quas optio perspiciatis voluptas pariatur cupiditate, odio sapiente, aspernatur
+              hic. Nulla aut vel sunt veniam autem quis nam ipsum quasi molestias velit! ea saepe quas optio perspiciatis
               voluptas pariatur cupiditate, odio sapiente, aspernatur hic. Nulla aut vel sunt veniam autem quis nam ipsum
               quasi molestias velit!</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem et necessitatibus, ea saepe quas optio perspiciatis
@@ -58,72 +62,63 @@
               voluptas pariatur cupiditate, odio sapiente, aspernatur hic. Nulla aut vel sunt veniam autem quis nam ipsum
               quasi molestias velit!</p>
           </div>
-
         </div>
+      </div>
+    </div>
+    <div class="container text-left">
+      <div class="col-xs-12">
+        <div class="row">
+          <article>
 
+            <h2 class="titulo line">Biografia</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur asperiores,
+              amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum repellat! Quaerat
+              illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum ullam. Harum accusamus
+              debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem, temporibus molestiae perspiciatis
+              error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis nostrum porro.
+            </p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur asperiores,
+              amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum repellat! Quaerat
+              illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum ullam. Harum accusamus
+              debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem, temporibus molestiae perspiciatis
+              error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis nostrum porro.
+            </p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur asperiores,
+              amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum repellat! Quaerat
+              illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum ullam. Harum accusamus
+              debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem, temporibus molestiae perspiciatis
+              error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis nostrum porro.
+            </p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur asperiores,
+              amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum repellat! Quaerat
+              illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum ullam. Harum accusamus
+              debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem, temporibus molestiae perspiciatis
+              error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis nostrum porro.
+            </p>
 
-
-        <div class="container">
-          <div class="col-xs-12">
-            <div class="row">
-              <article>
-                <h2 class="h1">Cruz e Souza</h1>
-                  <h2>Biografia</h2>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur
-                    asperiores, amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum
-                    repellat! Quaerat illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum
-                    ullam. Harum accusamus debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem,
-                    temporibus molestiae perspiciatis error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis
-                    nostrum porro.
-                  </p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur
-                    asperiores, amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum
-                    repellat! Quaerat illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum
-                    ullam. Harum accusamus debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem,
-                    temporibus molestiae perspiciatis error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis
-                    nostrum porro.
-                  </p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur
-                    asperiores, amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum
-                    repellat! Quaerat illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum
-                    ullam. Harum accusamus debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem,
-                    temporibus molestiae perspiciatis error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis
-                    nostrum porro.
-                  </p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur
-                    asperiores, amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum
-                    repellat! Quaerat illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum
-                    ullam. Harum accusamus debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem,
-                    temporibus molestiae perspiciatis error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis
-                    nostrum porro.
-                  </p>
-
-                  <h3>Obras do Autor</h3>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur
-                    asperiores, amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum
-                    repellat! Quaerat illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum
-                    ullam. Harum accusamus debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem,
-                    temporibus molestiae perspiciatis error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis
-                    nostrum porro.
-                  </p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur
-                    asperiores, amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum
-                    repellat! Quaerat illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum
-                    ullam. Harum accusamus debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem,
-                    temporibus molestiae perspiciatis error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis
-                    nostrum porro.
-                  </p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur
-                    asperiores, amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum
-                    repellat! Quaerat illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum
-                    ullam. Harum accusamus debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem,
-                    temporibus molestiae perspiciatis error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis
-                    nostrum porro.
-                  </p>
-            </div>
-          </div>
+            <h2 class="titulo line">Obras do Autor</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur asperiores,
+              amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum repellat! Quaerat
+              illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum ullam. Harum accusamus
+              debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem, temporibus molestiae perspiciatis
+              error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis nostrum porro.
+            </p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur asperiores,
+              amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum repellat! Quaerat
+              illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum ullam. Harum accusamus
+              debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem, temporibus molestiae perspiciatis
+              error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis nostrum porro.
+            </p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur asperiores,
+              amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur harum repellat! Quaerat
+              illo, repellat id, nulla, consequatur expedita fuga quod temporibus reiciendis dolorum ullam. Harum accusamus
+              debitis eligendi, nesciunt ad explicabo tenetur optio modi facilis magnam rem, temporibus molestiae perspiciatis
+              error, nisi nobis quidem quos! Illum consequuntur nesciunt perspiciatis nostrum porro.
+            </p>
           </article>
         </div>
+      </div>
+    </div>
 
 
 
