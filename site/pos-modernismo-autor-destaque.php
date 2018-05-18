bc1b8ef7d2661f010c3699414369e192d9748c74
Filipe Daineze <33236376+jimmyfilips@users.noreply.github.com>
2018-05-17 21:16:53 -0300

    Atualização principais obras pos modernismo

--- site/pos-modernismo-autor-destaque.php
+++ site/pos-modernismo-autor-destaque.php
@@ -38,19 +38,75 @@
         container se refere a classe usada pelo bootstrap
         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
         <article class="section container"> 
-            <h2>Em Produção</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+            <div class="row text-left">
+                <aside class="col-md-4 col-md-push-8">
+                    <h2 style="display: none">Dados do autor</h2>
+                    <div class="col-md-12 col-sm-6 text-center">
+                        <img src="img/simb/cruz-e-souza.jpg" alt="foto do autor" class="foto-autor">
+                    </div>
+                    
+                    <div class="col-md-12 col-sm-6 text-center">
+                        <table class="ficha-tecnica">
+                        <tr>
+                        <td>Nome completo</td>
+                        <td>Ariano Vilar Suassuna</td>
+                        </tr>
+                        <tr>
+                        <td>Nascimento</td>
+                        <td>16 de junho de 1927,<br>Cidade da Parahyba, Paraíba</td>
+                        </tr>
+                        <tr>
+                        <td>Morte</td>
+                        <td>23 de julho de 2014 (87 anos)<br>Recife, Pernambuco</td>
+                        </tr>
+                        <tr>
+                        <td>Alma mater</td>
+                        <td>Faculdade de Direito do Recife</td>
+                        </tr>
+                        <tr>
+                        <td>Ocupação</td>
+                        <td>Dramaturgo, romancista, ensaísta e poeta</td>
+                        </tr>
+                        <tr>
+                        <td>Prêmios</td>
+                        <td>Prêmio Nacional de Ficção (1973)</td>
+                        </tr>
+                        <tr>
+                        <td>Magnum opus</td>
+                        <td>Auto da Compadecida (1955)<br>Romance d'A Pedra do Reino e o Príncipe do Sangue do Vai-e-Volta (1971)</td>
+                        </tr>
+                        </table>
+                    </div>
+                </aside>
+                <article class="col-md-8 col-md-pull-4">
+                    <h2 class="titulo no-space text-center">Em Produção</h2>
+                    <div class="divisor"></div>
+                    <p class="mid-space">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
+                    eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
+                    totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
+                    minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+                    
+                    <p class="titulo title-left text-left mid-space">Sub-titulo</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
+                    eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
+                    totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
+                    minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
+                    eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
+                    totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
+                    minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+                    
+                    <p class="titulo title-left text-left mid-space">Sub-titulo</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
+                    eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
+                    totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
+                    minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
+                    eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
+                    totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
+                    minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+                </article>
+            </div>
         </article>
         
     </main>
