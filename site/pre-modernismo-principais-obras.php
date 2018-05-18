34b1227f83eff3a851fdc2b77597b48ad016e32d
Cleissonp <cleissonp@gmail.com>
2018-05-18 10:58:03 -0300

    Pre-Modernismo Bruna
    @bruna2802 Add paginas feito em Sala

--- site/pre-modernismo-principais-obras.php
+++ site/pre-modernismo-principais-obras.php
@@ -3,15 +3,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head> 
-   
+<head>
+
     <!-- Informações básicas
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
     <title>Brasil Literário - Pré-Modernismo: Principais Obras</title>
     <link rel="stylesheet" href="css/pre-modernismo.css">
     <?php include 'includes/estrutura/head.php'; ?>
-    
+
 </head>
 
 <body>
@@ -26,38 +26,124 @@
     $titulo="Pré-Modernismo";
     $subtitulo="Principais Obras"; 
     include 'includes/section-header.php'; 
-    ?>    
+    ?>
 
     <!-- Conteúdo
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <main>
         <h1 style="display: none">Conteúdo</h1>
-        
+
         <!-- Conteudo
         OBS: O Section define o espaçamento padrão de uma SESSÃO, 
         container se refere a classe usada pelo bootstrap
         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-        <article class="section container"> 
-            <h2>Em Produção</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+        <article class="section container">
+            <div class="row extra-space">
+                <div class="col-md-6 col-lg-6 text-justify">
+
+                    <h1 class="titulo cor-obras1">Os Sertões</h1>
+                    <h3 class="sub-titulo cor-sub-principais-obras1">Euclides da Cunha</h3>
+                    <div class="cor-texto-obras1">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias.</p>
+                    </div>
+
+                </div>
+                <div class="col-md-6 col-lg-6">
+
+                    <img src="img/pre-mord/1sertoes.jpg" alt="Capa de Livro" class="centralizar">
+                </div>
+            </div>
+            <div class="row extra-space">
+                <div class="col-md-6 col-lg-6 hidden-xs hidden-sm">
+                    <img src="img/pre-mord/2eu.jpg" alt="Capa de Livro">
+                </div>
+                <div class="col-md-6 col-lg-6 text-justify">
+                    <h1 class="titulo cor-obras2">Eu e Outras Poesias</h1>
+                    <h3 class="sub-titulo">Augusto dos Anjos</h3>
+                    <div class="cor-texto-obras1">
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6 hidden-lg hidden-md">
+                    <img src="img/pre-mord/2eu.jpg" alt="Capa de Livro">
+                </div>
+            </div>
+            <div class="row extra-space">
+                <div class="col-md-6 col-lg-6  text-justify">
+
+                    <h1 class="titulo cor-obras3">Triste Fim de Policarpo Quaresma</h1>
+                    <h3 class="sub-titulo cor-sub-principais-obras3">Lima Barreto</h3>
+                    <div class="cor-texto-obras1">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6">
+                    <img src="img/pre-mord/3tristefim.jpg" alt="Capa de Livro">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-lg-6 hidden-xs hidden-sm">
+                    <img src="img/pre-mord/4urupes.jpg" alt="Capa de Livro">
+                </div>
+                <div class="col-md-6 col-lg-6 text-justify">
+                    <h1 class="titulo">Urupês</h1>
+                    <h3 class="sub-titulo">Monteiro Lobato</h3>
+                    <div class="cor-texto-obras1">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias, consequatur delectus? Nam ut, perferendis
+                            repellendus cum quo quas omnis tenetur excepturi laborum est saepe!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt ducimus natus culpa
+                            cum dolorum quia similique, iste ullam, molestias.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6 hidden-md hidden-lg">
+                    <img src="img/pre-mord/4urupes.jpg" alt="Capa de Livro">
+                </div>
+            </div>
         </article>
-        
+
     </main>
 
     <!-- Rodapé
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <?php include 'includes/estrutura/footer.php'; ?>   
+    <?php include 'includes/estrutura/footer.php'; ?>
 
 </body>
 
