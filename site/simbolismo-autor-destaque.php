2dffa6a8afa5b9ee7d048736c185e9f3eee24ec3
Cleissonp <cleissonp@gmail.com>
2018-05-18 12:20:20 -0300

    Minificado

--- site/simbolismo-autor-destaque.php
+++ site/simbolismo-autor-destaque.php
@@ -1,59 +1,35 @@
 <?php include 'includes/estrutura/assinatura.php'; ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-
-    <!-- Informações básicas
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
     <title>Brasil Literário - Simbolismo: Autor de Destaque</title>
     <?php include 'includes/estrutura/head.php'; ?>
     <link rel="stylesheet" href="css/simbolismo.css">
-    <link href="https://fonts.googleapis.com/css?family=Domine" rel="stylesheet">
-
-
 </head>
-
 <body>
-
-    <!-- Menu de Navegação
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <?php include 'includes/estrutura/navbar.php'; ?>
-
-    <!-- Header
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <?php $titulo='Simbolismo'; 
   // $subtitulo='Cruz e Souza';
    include 'includes/section-header.php'; ?>
-
-    <!-- Conteúdo
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <main>
         <h1 style="display: none">Conteúdo</h1>
-
-        <!-- Escolas Literárias
-        –––––––––––––––––––––––––––––––––––––––––––––––––– -->
         <aside>
             <div class="container">
                 <div class="section">
                     <div class="row">
-
                         <h2 class="titulo text-center cor">Autor de Destaque</h2>
                         <div class="SI divisor"></div>
-                        <!-- <hr class="hidden-xs"> -->
                         <div class="media">
                             <div class="media-left media-middle">
-                                
                                 <img class="media-object " src="img/simb/cruz-e-souza.jpg" alt="Autor de Destaque">
-                                <cite><small>João Cruz e Sousa: Ilustração Digital relizado por Cleisson</small></cite>
+                                <cite>
+                                    <small>João Cruz e Sousa: Ilustração Digital relizado por Cleisson</small>
+                                </cite>
                             </div>
-
                             <div class="media-body media-middle hidden-xs">
                                 <p class="titulo text-center cor">Cruz e Sousa, o cisne negro, principal precursor do simbolismo no Brasil.</p>
                                 <p class="text-center">
-
                                     "Filho de negros alforriados, João da Cruz ficou sob tutela da família do militar Guilherme Xavier de Sousa. Recebeu boa
                                     educação e combateu arduamente a escravidão e o preconceito racial no jornal Tribuna
                                     Popular"
@@ -64,24 +40,20 @@
                                 <hr>
                                 <p class="titulo cor text-center">Cruz e Sousa, o cisne negro, principal precursor do simbolismo no Brasil.</p>
                                 <p class="text-center">
-
                                     "Filho de negros alforriados, João da Cruz ficou sob tutela da família do militar Guilherme Xavier de Sousa. Recebeu boa
                                     educação e combateu arduamente a escravidão e o preconceito racial no jornal Tribuna
                                     Popular"
                                 </p>
-
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </aside>
         <div class="container text-left">
             <div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
                 <div class="row">
                     <article>
-
                         <h2 class="titulo cor line">Biografia</h2>
                         <p> Perspiciatis inventore ut veritatis consequuntur asperiores, amet, perferendis alias enim consectetur
                             distinctio, maiores reprehenderit consequatur harum repellat!Quaerat illo, repellat id, nulla,
@@ -111,7 +83,6 @@
                             magnam rem, temporibus molestiae perspiciatis error, nisi nobis quidem quos! Illum consequuntur
                             nesciunt perspiciatis nostrum porro.
                         </p>
-
                         <h2 class="titulo cor line">Obras do Autor</h2>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis inventore ut veritatis consequuntur
                             asperiores, amet, perferendis alias enim consectetur distinctio, maiores reprehenderit consequatur
@@ -136,20 +107,11 @@
                         </p>
                         <div class="section"></div>
                     </article>
-
                 </div>
             </div>
         </div>
-
-
-
-
     </main>
-
-    <!-- Rodapé
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <?php include 'includes/estrutura/footer.php'; ?>
-
 </body>
 
 </html>
