397df989f48339d292c02b2c63b02d45e0ff8863
Cleissonp <cleissonp@gmail.com>
2018-05-19 15:49:53 -0300

    Ajuste Final

--- site/simbolismo-contexto-historico.php
+++ site/simbolismo-contexto-historico.php
@@ -35,25 +35,25 @@
         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
         <article class="section container">
 
-            <div class="col-md-12">
+            <div class="col-md-11 col-md-offset-1 ">
                 <div class="row">
                     <h2 class="titulo text-center">Simbolismo </h2>
                     <div class="SI divisor"></div>
-                    <div class="col-md-6 text-left">
+                    <div class="col-md-8 text-left">
                         <h3 class="titulo line">Origem</h3>
                         <p class="capitular"> Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
                     </div>
-                    <div class="col-md-4 col-xs-10 col-xs-offset-1 text-center">
+                    <div class="col-md-4 text-center">
                         <img src='https://upload.wikimedia.org/wikipedia/commons/1/18/Henri_de_Toulouse-Lautrec_005.jpg' alt='' class="imgcenter" />
                         <cite>
                             <small class="text-center">Henri de Toulouse-Lautrec: At the Moulin Rouge, The Dance</small>
-                        </cite>
+                     <br>  Simbolismo Iniciado na Belle Epóque Francesa  </cite>
                     </div>
                     <hr class="visible-xs">
                 </div>
                 <div class="row">
-                    <div class="col-md-6 text-left">
+                    <div class="col-md-8 text-left">
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
                         <h3 class="titulo line">Surgimento</h3>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
@@ -63,34 +63,32 @@
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
                     </div>
                 </div>
-                <div class="col-md-12">
-                    <div class="row">
-                        <h2 class="titulo text-center">Simbolismo no Brasil </h2>
-                        <div class="SI divisor"></div>
-                        <div class="col-md-6 text-left">
-                            <h3 class="titulo line">Contexto Cultural</h3>
+            </div>
 
-                            <p class="capitular">Magni! placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-                        </div>
-                        <div class="col-md-4 col-xs-10 col-xs-offset-1 text-center">
-                            <img src='https://ogimg.infoglobo.com.br/in/15402412-60b-ac7/FT1086A/420/x2009-269789614-2009041547241.jpg_20090415.jpg.pagespeed.ic.yEPCoTOcvV.jpg' alt='' class="imgcenter" />
-                            <cite>
-                            <small class="text-center">Castigo de Escravo: Pintura de Jean Baptiste Debret</small>
+            <div class="col-md-11 col-md-offset-1 ">
+                <div class="row">
+                    <h2 class="titulo text-center">Simbolismo no Brasil </h2>
+                    <div class="SI divisor"></div>
+                    <div class="col-md-8 text-left">
+                        <h3 class="titulo line">Contexto Cultural</h3>
+                        <p class="capitular">Ex iure ducimus, Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, deserunt dolorum esse sed.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <img src='https://ogimg.infoglobo.com.br/in/15402412-60b-ac7/FT1086A/420/x2009-269789614-2009041547241.jpg_20090415.jpg.pagespeed.ic.yEPCoTOcvV.jpg' alt='' class="imgcenter" />
+                        <cite>
+                            <small class="text-center">Pintura de Jean Baptiste Debret:Castigo de Escravo</small><br>No Brasil, o Simbolismo iniciou no final do Século XIX
                         </cite>
-                        </div>
                     </div>
                     <hr class="visible-xs">
-                    <div class="row">
-                        <div class="col-md-6 text-left">
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-                            <h3 class="titulo line">Contexto Histórico</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-                        </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-8 text-left">
+                        <p>Fugit lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+                        <h3 class="titulo line">Surgimento</h3>
+                        <p>Suscipit, lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
                     </div>
                 </div>
-            </div>
             </div>
         </article>
 
