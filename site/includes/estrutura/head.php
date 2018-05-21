e2619f7b717f3491b1463f41c8ccb75f489cf0ca
Filipe Daineze <jimmyfilips@gmail.com>
2018-05-21 12:55:24 -0300

    correcao head

--- site/includes/estrutura/head.php
+++ site/includes/estrutura/head.php
@@ -22,10 +22,6 @@
 
     <!-- Bootstrap
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <!--
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.min.js"></script>
     
@@ -35,7 +31,7 @@
     <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet">
     <!-- textos -->
     <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
-
+    <!-- textos -->
     <link href="https://fonts.googleapis.com/css?family=Zilla+Slab" rel="stylesheet">
     
     <!-- CSS
