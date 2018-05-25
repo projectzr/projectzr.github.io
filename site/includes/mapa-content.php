89cda0230cda2dfd1ab97e4d1b8842a2b7cbd26e
Beto Salgado <33236587+betosalgado@users.noreply.github.com>
2018-05-24 22:07:23 -0300

    Add files via upload

---
+++ site/includes/mapa-content.php
@@ -0,0 +1,114 @@
+<article class="container section">
+    <div class="row text-left">
+        <div class="col-xs-12 mp-mapa">
+
+            <div class="col-xs-12 mp-home">
+                <h2>Navegue no Brasil literário</h2>
+            </div>
+            <div class="col-xs-12 mp-escolasgeral">
+                <h3 class="mp-molduraescola">Escolas <span class="caret"></span>
+
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6 col-lg-4 mp-escola ">
+                            <h4> Romantismo <span class="caret"></span></h4>
+                            <ul>
+                                <li><a class="mp-link" href="romantismo-contexto-historico.php">Romantismo e a Independência</a></li>
+                                <li><a class="mp-link" href="romantismo-principais-obras.php">As obras mais importantes</a></li>
+                                <li><a class="mp-link" href="romantismo-autor-destaque.php">Autor de destaque: José de Alencar</a></li>
+                            </ul>
+
+                        </div>
+
+                        <div class="col-xs-12 col-md-6 col-lg-4 mp-escola">
+                            <h4> Realismo <span class="caret"></span></h4>
+
+
+                            <ul>
+                                <li><a class="mp-link" href="realismo-contexto-historico.php">O fim da Monarquia</a></li>
+                                <li><a class="mp-link" href="realismo-principais-obras.php">O Realismo no Brasil</a></li>
+                                <li><a class="mp-link" href="realismo-autor-destaque.php">Autor de destaque: Machado de Assis</a></li>
+                            </ul>
+
+
+
+                        </div>
+
+                        <div class="col-xs-12 col-md-6 col-lg-4 mp-escola">
+                            <h4> Simbolismo <span class="caret"></span></h4>
+                            <ul>
+                                <li><a class="mp-link" href="realismo-contexto-historico.php">O Início da Bell Epoque</a></li>
+                                <li><a class="mp-link" href="realismo-principais-obras.php">Obras da Época</a></li>
+                                <li><a class="mp-link" href="simbolismo-autor-destaque.php">Autor de destaque: Cruz e Sousa</a></li>
+                            </ul>
+
+                        </div>
+
+                        <div class="col-xs-12 col-md-6 col-lg-4 mp-escola">
+                            <h4> Pré-Modernismo <span class="caret"></span></h4>
+                            <ul>
+                                <li><a class="mp-link" href="pre-modernismo-contexto-historico.php">Proclamação da República</a></li>
+                                <li><a class="mp-link" href="pre-modernismo-principais-obras.php">Manifestação dos Autores</li>
+                                <li><a class="mp-link" href="pre-modernismo-autor-destaque.php">Autor de destaque: Euclides da Cunha</a></li>
+                            </ul>
+
+                        </div>
+                        <div class="col-xs-12 col-md-6 col-lg-4 mp-escola">
+                            <h4> Modernismo <span class="caret"></span></h4>
+                            <ul>
+                                <li><a class="mp-link" href="modernismo-contexto-historico.php">Semana da Arte Moderna</a></li>
+                                <li><a class="mp-link" href="modernismo-principais-obras.php">Principais Obras</a></li>
+                                <li><a class="mp-link" href="modernismo-autor-destaque.php">Autor de destaque: Mario de Andrade</a></li>
+                            </ul>
+
+                        </div>
+                        <div class="col-xs-12 col-md-6 col-lg-4 mp-escola">
+                            <h4> Pós-Modernismo <span class="caret"></span></h4>
+                            <ul>
+                                <li><a class="mp-link" href="pos-modernismo-contexto-historico.php">A Guerra, A História</a></li>
+                                <li><a class="mp-link" href="pos-modernismo-principais-obras.php">Obras que Marcaram</a></li>
+                                <li><a class="mp-link" href="pos-modernismo-autor-destaque.php">Autor de destaque: Guimarães Rosa</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+
+                    </li>
+            </div>
+
+
+            <div class="row">
+
+                <div class="col-xs-4 mp-agendaecontato">
+                    <ul>
+                        <li><a class="mp-link" href="agenda.php">Agenda</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-4 mp-agendaecontato">
+                    <ul>
+                        <li><a class="mp-link" href="contato.php">Contato</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-4 mp-institucional">
+                <h4> Intitucional <span class="caret"></span></h4>
+                <ul>
+                    <li><a class="mp-link" href="quem-somos.php">Quem somos</a></li>
+                    <li><a class="mp-link" href="dicas-de-vestibular.php">Dicas de Vestibular</a></li>
+                    <li><a class="mp-link" href="academia.php">Academia Brasileira de Letras</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-4 mp-institucional">
+                <h4> <span class="caret"></span></h4>
+
+                <ul>
+
+                    <li><a class="mp-link" href="mapa.php">Mapa do Site</a></li>
+                    <li><a class="mp-link" href="normas.php">Normas de Utilização</a></li>
+                    <li><a class="mp-link" href="index.php">Home</a></li>
+                </ul>
+            </div>
+        </div>
+        </h3>
+    </div>
+    </div>
+</article>
