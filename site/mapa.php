4cc2d3447c218974561004084772b73c64b25fed
Beto Salgado <33236587+betosalgado@users.noreply.github.com>
2018-05-24 22:06:48 -0300

    Add files via upload

--- site/mapa.php
+++ site/mapa.php
@@ -10,7 +10,7 @@
     <meta charset="utf-8">
     <title>Brasil Literário - Contato</title>
     <?php include 'includes/estrutura/head.php'; ?>
-
+    <link rel="stylesheet" href="css/mapa.css">
 </head>
 
 <body>
@@ -25,77 +25,11 @@
 
     <!-- Conteúdo
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <article>
+    <main>
         <h1 style="display: none">Conteúdo</h1>
-
-        <div class="container section">
-            <div class="row text-left">
-                <div class="col-xs-12">
-                    <ul>
-                        <li>Home</li>
-                        <li>Escolas <span class="caret"></span>
-                            <ul>
-                                <li>Romantismo <span class="caret"></span>
-                                    <ul>
-                                        <li>Contexto Histórico (titulo a definir)</li>
-                                        <li>Principais Obras (titulo a definir)</li>
-                                        <li>Autor de destaque: José de Alencar</li>
-                                    </ul>
-                                </li>
-                                <li>Realismo <span class="caret"></span>
-                                    <ul>
-                                        <li>O Fim da Monarquia</li>
-                                        <li>O Realismo no Brasil</li>
-                                        <li>Autor de destaque: Machado de Assis</li>
-                                    </ul>
-                                </li>
-                                <li>Simbolismo <span class="caret"></span>
-                                    <ul>
-                                        <li>O Início da Bell Epoque</li>
-                                        <li>Obras da Época</li>
-                                        <li>Autor de destaque: Cruz e Sousa</li>
-                                    </ul>
-                                </li>
-                                <li>Pré-Modernismo <span class="caret"></span>
-                                    <ul>
-                                        <li>Proclamação da República</li>
-                                        <li>Manifestação dos Autores</li>
-                                        <li>Autor de destaque: Euclides da Cunha</li>
-                                    </ul>
-                                </li>
-                                <li>Modernismo <span class="caret"></span>
-                                    <ul>
-                                        <li>Semana da Arte Moderna</li>
-                                        <li>Principais Obras</li>
-                                        <li>Autor de destaque: Mario de Andrade</li>
-                                    </ul>
-                                </li>
-                                <li>Pós-Modernismo <span class="caret"></span>
-                                    <ul>
-                                        <li>A Guerra, A História</li>
-                                        <li>Obras que Marcaram</li>
-                                        <li>Autor de destaque: Guimarães Rosa</li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>Agenda</li>
-                        <li>Contato</li>
-                        <li>Institucional <span class="caret"></span>
-                            <ul>
-                                <li>Quem somos</li>
-                                <li>Dicas de Vestibular</li>
-                                <li>Academia Brasileira de Letras</li>
-                                <li>Mapa do Site</li>
-                                <li>Normas de Utilização</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-    </article>
+        
+        <?php include 'includes/mapa-content.php'; ?>
+    </main>
 
     <!-- Rodapé
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
