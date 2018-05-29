bfb650617677cbebeb38663c2852ec5f61987f3a
Filipe Daineze <jimmyfilips@gmail.com>
2018-05-28 21:52:36 -0300

    contexto historico - posmodernismo (incompleto)

--- site/pos-modernismo-contexto-historico.php
+++ site/pos-modernismo-contexto-historico.php
@@ -8,7 +8,7 @@
     <!-- Informações básicas
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
-    <title>Brasil Literário - Pós-Modernismo: Contexto Histórico</title>
+    <title>Brasil Literário - Pós-Modernismo: Autor de Destaque</title>
     <link rel="stylesheet" href="css/pos-modernismo.css">
     <?php include 'includes/estrutura/head.php'; ?>
     
@@ -24,34 +24,47 @@
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <?php 
     $titulo="Pós-Modernismo";
-    $subtitulo="Contexto Histórico"; 
+    $subtitulo="Autor de Destaque"; 
     include 'includes/section-header.php'; 
     ?>    
 
     <!-- Conteúdo
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <main>
-        <h1 style="display: none">Conteúdo</h1>
-        
-        <!-- Conteudo
-        OBS: O Section define o espaçamento padrão de uma SESSÃO, 
-        container se refere a classe usada pelo bootstrap
-        –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-        <article class="section container"> 
-            <h2>Em Produção</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
-            eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
-            totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
-            minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
-        </article>
+        <h1 style="display: none">Contexto Histórico</h1>
+       
+        <div class="section container"> 
+            
+            <article class="col-xs-12">
+                <img src="img/pos-modernismo/contexto-banner.old.jpg" style="width: 100%;" alt="zona de guerra decada de 40">
+                
+                <h2 class="titulo no-space text-center">A Guerra, A História</h2>
+                <div class="divisor"></div>
+                
+                <div class="coluna-esquerda col-md-6">
+                    <blockquote class="mid-space contexto-bq">
+                        <p class="text-justify">"Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe Magni!"</p>
+                    </blockquote>
+                    <p class="text-justify first-letter">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed.</p>
+                    <p class="text-justify mid-space ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
+                    eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
+                    totam nulla ducimus labore. Magni!</p>
+                </div>
+
+                <div class="coluna-direita col-md-6">
+                    <div id="spacer"></div>
+                    <p class="titulo title-left text-justify mid-space line-correction">O Pós-Modernismo</p>
+                    <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
+                    eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
+                    totam nulla ducimus labore. Magni!</p>
+                    <p class="mid-space text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, iste molestias? Fugit quisquam corporis repellendus
+                    eligendi inventore tempora vero sapiente minus quaerat magni magnam suscipit quia quod, assumenda saepe voluptatibus
+                    totam nulla ducimus labore. Magni! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat cum
+                    minima provident impedit mollitia nobis soluta debitis illo, ex iure ducimus, deserunt dolorum esse sed totam nulla ducimus labore. Magni!</p>
+                </div>
+            </article>
+            
+        </div>
         
     </main>
 
